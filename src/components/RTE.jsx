c1b955087d8ea4c0cb452343eed7042ff7d583a5
mounishvatti <mounishvatti2002@gmail.com>
2024-08-19 16:25:51 +0530

    fixed few bugs

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -19,7 +19,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
               initialValue: defaultValue,
               height: 500,
               menubar: true,
-              plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+              plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
               toolbar:'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
               content_style:
                 "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
