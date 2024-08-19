7184c2cc4d61bf5ef9441a3a26bef8cb65eb610e
mounishvatti <mounishvatti2002@gmail.com>
2024-08-19 17:33:47 +0530

    added onClick on logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Logo() {
+  const navigate = useNavigate()
   return (
     <img src='https://cdn.iconscout.com/icon/free/png-256/free-blog-logo-icon-download-in-svg-png-gif-file-formats--blogger-blogspot-post-web-social-media-pack-logos-icons-1596827.png'
-    alt='logo' width={50}/>
+    alt='logo' width={50}
+    onClick={()=>navigate('/')}
+    />
   )
 }
 
